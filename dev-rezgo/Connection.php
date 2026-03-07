fb82d7f2a4b0cfa2aea218045d0ada02321b1a12
faisalkhalid09 <faisal3245.com@gmail.com>
2026-03-07 19:19:06 +0500

    Set DB password to empty

--- dev-rezgo/Connection.php
+++ dev-rezgo/Connection.php
@@ -2,7 +2,7 @@
 // Live VPS Database Credentials
 $db_host = 'localhost';
 $db_user = 'root';
-$db_pass = 'Faisalkhalid1#';
+$db_pass = '';
 $db_name = 'devrezgo';
 
 $db = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
