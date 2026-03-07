bab819a4c624159d1c7e9f77e7a07a07d780f915
faisalkhalid09 <faisal3245.com@gmail.com>
2026-03-07 19:15:05 +0500

    Update Connection.php with live credentials

--- dev-rezgo/Connection.php
+++ dev-rezgo/Connection.php
@@ -1,9 +1,9 @@
 <?php
 // Live VPS Database Credentials
 $db_host = 'localhost';
-$db_user = 'ENTER_LIVE_DB_USER_HERE';
-$db_pass = 'ENTER_LIVE_DB_PASS_HERE';
-$db_name = 'ENTER_LIVE_DB_NAME_HERE';
+$db_user = 'root';
+$db_pass = 'Faisalkhalid1#';
+$db_name = 'devrezgo';
 
 $db = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
 
