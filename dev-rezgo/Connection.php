989fab13ca6d7d832c432873a6f61a4aa6e68fc2
faisalkhalid09 <faisal3245.com@gmail.com>
2026-03-07 19:21:48 +0500

    Switch to dedicated devrezgo_user for security

--- dev-rezgo/Connection.php
+++ dev-rezgo/Connection.php
@@ -1,8 +1,8 @@
 <?php
 // Live VPS Database Credentials
 $db_host = 'localhost';
-$db_user = 'root';
-$db_pass = '';
+$db_user = 'devrezgo_user';
+$db_pass = 'Faisalkhalid1#';
 $db_name = 'devrezgo';
 
 $db = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
