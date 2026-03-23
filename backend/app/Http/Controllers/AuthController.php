2e9b6ac4d1b1c41855126989ec370bf557d8db68
faisalkhalid09 <faisal3245.com@gmail.com>
2026-03-23 19:49:47 +0500

    Store update

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -21,7 +21,7 @@ class AuthController extends Controller
     public function login(Request $request)
     {
         $credentials = $request->validate([
-            'email'    => ['required', 'email'],
+            'email'    => ['required', 'email:rfc,dns'],
             'password' => ['required'],
         ]);
 
@@ -52,7 +52,7 @@ class AuthController extends Controller
     {
         $data = $request->validate([
             'name'                  => ['required', 'string', 'max:255'],
-            'email'                 => ['required', 'email', 'unique:users'],
+            'email'                 => ['required', 'email:rfc,dns', 'unique:users'],
             'password'              => ['required', 'min:8', 'confirmed'],
         ]);
 
