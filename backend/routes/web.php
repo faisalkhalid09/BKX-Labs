68a612843ea4097d4b6da4e1252e254a234069a0
faisalkhalid09 <faisal3245.com@gmail.com>
2026-03-25 23:23:27 +0500

    Store update

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -14,6 +14,7 @@ Route::get('/', function () {
 });
 
 // Google Auth Routes
+Route::get('/debug-route', function() { return "Debug OK"; });
 Route::get('/auth/google/redirect', [\App\Http\Controllers\Auth\GoogleAuthController::class, 'redirectToGoogle'])->name('auth.google.redirect');
 Route::get('/auth/google/callback', [\App\Http\Controllers\Auth\GoogleAuthController::class, 'handleGoogleCallback'])->name('auth.google.callback');
 
