d13367cfab0cfa946c723b571983021ded5a096a
faisalkhalid09 <faisal3245.com@gmail.com>
2026-04-18 18:01:44 +0500

    successful otimization for fast

--- tools-web/next.config.ts
+++ tools-web/next.config.ts
@@ -1,8 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  basePath: '/tools',
-  assetPrefix: '/tools/',
+  /* config options here */
 };
 
 export default nextConfig;
